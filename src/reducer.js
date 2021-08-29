14d68a47b0ef172d2f824a77a150d1e7655b9505
pooriyapfn <pooriyapfn@gmail.com>
2021-08-29 12:59:20 +0430

    Update reducer.js
    switch case was incorrect syntax

--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ import * as actions from './actionTypes'
 let lastId = 0
 
 export default function reducer(state = [], action) {
-    switch (action.type === 'bugAdded') {
+    switch (action.type) {
         case actions.BUG_ADDED:
             return [
                 ...state,
